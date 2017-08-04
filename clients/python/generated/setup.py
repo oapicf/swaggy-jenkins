3df533f05402b89e11e91dacc93f66b772540487
Cliffano Subagio <cliffano@gmail.com>
2017-08-04 13:09:48 +1000

    [python] Bump version up to 0.0.3 .

--- clients/python/generated/setup.py
+++ clients/python/generated/setup.py
@@ -15,7 +15,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "swaggyjenkins"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 # To install the library, run the following
 #
 # python setup.py install
--- clients/python/generated/swaggyjenkins/api_client.py
+++ clients/python/generated/swaggyjenkins/api_client.py
@@ -71,7 +71,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/0.0.2/python'
+        self.user_agent = 'Swagger-Codegen/0.0.3/python'
 
     @property
     def user_agent(self):
