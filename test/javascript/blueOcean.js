c05be8879f25b5bcecfc208657cfd36873e5138a
Cliffano Subagio <cliffano@gmail.com>
2018-08-21 14:05:59 +1000

    Add integration tests.

---
+++ test/javascript/blueOcean.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const Swaggy = require('swaggy-jenkins');
+const blueOceanApi = new Swaggy.BlueOceanApi();
+blueOceanApi.apiClient.basePath = 'https://ci.eclipse.org/ecf/';
+
+describe('remote access', function() {
+  describe('without format', function() {
+    it('should return error when Jenkins instance has not installed Blue Ocean plugin', function(done) {
+      var callback = function(error, data, response) {
+        assert.equal(error.status, 404);
+        assert.equal(data, null);
+        assert.equal(response.res.statusCode, 404);
+        done();
+      };
+      blueOceanApi.getOrganisations(callback);
+    });
+  });
+});
---
+++ test/javascript/remoteAccess.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const Swaggy = require('swaggy-jenkins');
+const remoteAccessApi = new Swaggy.RemoteAccessApi();
+remoteAccessApi.apiClient.basePath = 'https://ci.eclipse.org/ecf/';
+
+describe('remote access', function() {
+  describe('get Jenkins', function() {
+    it('should return response model', function(done) {
+      var callback = function(error, data, response) {
+        assert.equal(error, null);
+        assert.equal(typeof data, 'object');
+        assert.equal(response.res.statusCode, 200);
+        done();
+      };
+      remoteAccessApi.getJenkins(callback);
+    });
+  });
+});
