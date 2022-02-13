af3bddee7e7e9c2ea5f73a448ff7d8755cd1b5d0
Cliffano Subagio <cliffano@gmail.com>
2022-02-13 22:39:39 +1100

    Add publish-lang GH Actions.

---
+++ .github/actions/publish-javascript-action/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make deps publish-javascript
---
+++ .github/actions/publish-ruby-action/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make deps
+mkdir -p ~/.gem/
+touch ~/.gem/credentials
+echo "---" >> ~/.gem/credentials
+echo ":rubygems_api_key: ${RUBYGEMS_TOKEN}" >> ~/.gem/credentials
+chmod 0600 ~/.gem/credentials
+make publish-ruby
