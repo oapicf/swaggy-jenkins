975c27dbf17c8be35e50f2c3bae8edb8b61a91b7
Cliffano Subagio <cliffano@gmail.com>
2022-02-21 21:24:51 +1100

    Fix missing npmrc provisioning on JavaScript publishing.

--- .github/actions/publish-javascript-action/entrypoint.sh
+++ .github/actions/publish-javascript-action/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 make deps generate-primary
 
-echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
-echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
-echo "always-auth=true" >> ~/.npmrc
-
 export NODE_AUTH_TOKEN="${NPMJS_TOKEN}"
+echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+echo "registry=https://registry.npmjs.org/" > ~/.npmrc
+echo "always-auth=true" > ~/.npmrc
 make --environment-overrides publish-javascript
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,10 +6,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Fixed
+- Fix missing npmrc provisioning on JavaScript publishing
 
 ## 1.3.2 - 2022-02-21
 ### Fixed
-- Fix missing npmrc provisioning on JavaScript publishing
 - Fix generate-primary sub-call to generate-langs-primary
 
 ## 1.3.1 - 2022-02-21
