67590627d17c50a9233b0843a7a6c2d8851a7b85
Cliffano Subagio <cliffano@gmail.com>
2025-11-18 01:40:22 +1100

    Regenerating primary clients as part of publish-javascript until issue #58 is resolved at OAG.

--- .github/actions/publish-javascript-action/entrypoint.sh
+++ .github/actions/publish-javascript-action/entrypoint.sh
@@ -3,7 +3,9 @@ set -o errexit
 set -o nounset
 
 export NODE_AUTH_TOKEN="${NPMJS_TOKEN}"
-make clean deps init-spec generate-primary build-javascript test-javascript
+# TODO: re-enable make clean and generate-primary once issue #58 is resolved in OAG
+# make clean deps init-spec generate-primary build-javascript test-javascript
+make deps init-spec build-javascript test-javascript
 
 rm -f ~/.npmrc && touch ~/.npmrc
 echo "Provisioning npm config..."
